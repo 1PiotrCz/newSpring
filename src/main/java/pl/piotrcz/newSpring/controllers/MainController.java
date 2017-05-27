f5d6028f87827c3bd2bb66876f6da8dabbc61094
Piotr Czubkowski <piotrcz@o2.pl>
2017-05-27 17:54:46 +0200

    change banner, add some form

--- src/main/java/pl/piotrcz/newSpring/controllers/MainController.java
+++ src/main/java/pl/piotrcz/newSpring/controllers/MainController.java
@@ -5,7 +5,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import pl.piotrcz.newSpring.models.SimpleBean;
+
+import java.time.LocalDateTime;
 
 /**
  * Created by Piotr Czubkowski on 2017-05-27.
@@ -24,11 +28,27 @@ public class MainController {
 //        return "<b>Hello POST</b>";
 //    }
 
-    @RequestMapping(value = "/", method = RequestMethod.GET )
+    @RequestMapping(value = "/", method = RequestMethod.GET)
 //    @ResponseBody
-    public String mainGet(Model model){
-        model.addAttribute("someString", "Pochodze z controllera");
+    public String mainGet(Model model) {
+        LocalDateTime now = LocalDateTime.now();
+
+        System.out.println("Time now" + now.toString());
+
+        if (now.getHour() >= 16) {
+            model.addAttribute("someString", "a");
+        } else {
+            model.addAttribute("someString", "b");
+        }
         return "index";
+    }
+
+    @RequestMapping(value = "/data", method = RequestMethod.POST)
+    @ResponseBody
+    public String data(@RequestParam(value = "name") String name,
+                       @RequestParam(value = "lastName") String lastName,
+                       @RequestParam(value = "age", required = false) int age) {
+        return name + " " + lastName + " Jest " + ((age > 17) ? " pelnoletni" : " niepelnoletni");
     }
 
 }
